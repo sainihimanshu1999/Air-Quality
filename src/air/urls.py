88cbb6c8a9bf3938852fcff0d7e865f951261cc0
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-25 16:39:53 +0530

    complete

--- src/air/urls.py
+++ src/air/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name = "home"),
-    path('about.html', views.about, name = "about")
+   
 
     
     
--- src/air/views.py
+++ src/air/views.py
@@ -1,9 +1,40 @@
+import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
 
 def home(request):
-    return render(request , 'home.html', {})
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=New Delhi&appid=4ee1e71fed9417ca29e961469d9ee039'
+    city = 'New Delhi'
+
+    if request.method == 'POST':
+        form  = CityForm(request.POST or None)
+        form.save()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temprature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+  
+
+    context = {'weather_data' : city_weather, 'form': form }
+
+    return render(request , 'home.html', context)
 
 
 
-def about(request):
-    return render(request , 'about.html', {})
